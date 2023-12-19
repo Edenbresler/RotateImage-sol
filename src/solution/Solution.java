b55b0de5121e26d68e9986b1d29d2e5949aec72e
Edenbresler <150347397+Edenbresler@users.noreply.github.com>
2023-12-19 20:50:03 +0200

    Update Solution.java

--- src/solution/Solution.java
+++ src/solution/Solution.java
@@ -1,2 +1,29 @@
-package solution;public class Solution {
+package solution;
+
+public class Solution {
+    public static void main(String[] args) {
+        // we need to print 741852963
+        int[][] example1 = {{1, 2, 3}, {4, 5, 6}, {7, 8, 9}};
+        rotate(example1);
+        for (int i = 0; i < example1.length; i++) {
+            for (int j = 0; j < example1[0].length; j++) {
+                System.out.print(example1[i][j]);
+            }
+        }
+
+
+    }
+
+    public static void rotate(int[][] matrix) {
+        int size = matrix.length;
+        for (int i = 0; i < (matrix.length / 2); i++) {
+            for (int j = i; j < (size - 1 - i); j++) {
+                int temp = matrix[i][j];
+                matrix[i][j] = matrix[size - 1 - j][i];
+                matrix[size - 1 - j][i] = matrix[size - 1 - i][size - 1 - j];
+                matrix[size - 1 - i][size - 1 - j] = matrix[j][size - 1 - i];
+                matrix[j][size - 1 - i] = temp;
+            }
+        }
+    }
 }
